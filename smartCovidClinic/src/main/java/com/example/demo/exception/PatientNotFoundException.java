5b46704af0100a884b8c0974f67f18a27dff6788
Siddartha-Gurugubelli <79105336+Siddartha-Gurugubelli@users.noreply.github.com>
2022-09-27 16:35:56 +0530

    Add files via upload

---
+++ smartCovidClinic/src/main/java/com/example/demo/exception/PatientNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.demo.exception;
+
+@SuppressWarnings("serial")
+public class PatientNotFoundException extends Exception {
+	
+	public PatientNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
